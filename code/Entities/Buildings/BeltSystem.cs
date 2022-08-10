c31882e58c3f625c3f5d7a970d5ef6be4e01a2f4
jsmith128 <70296489+jsmith128@users.noreply.github.com>
2022-08-10 04:19:26 -0400

    Update BeltSystem.cs

--- code/Entities/Buildings/BeltSystem.cs
+++ code/Entities/Buildings/BeltSystem.cs
@@ -6,22 +6,21 @@ partial class BeltSystem : Entity
 	//public List<Entity> ItemIDs => new List<Entity>();
 
 
-	// The positions relative to center of tile of where items need to end up.
-	// When they reach this point, give them the waypoint of the next belt on the correct side.
-	// These should be on the very edge of the tile.
-	// Convert this to world coordinates before giving it to item.
-	public Vector3 LLocalWaypoint = new Vector3 ( 0, 0, 0 );
-	public Vector3 RLocalWaypoint = new Vector3 ( 0, 0, 0 );
-
 	// Offset of each belt in the belt system, at the center
 	public Dictionary<TransportBelt, float> BeltOffsets = new Dictionary<TransportBelt, float> ();
+	public Dictionary<string, float> Items = new Dictionary<string, float> ();
 
 	public void PlaceItem( TransportBelt belt, string ClassName )
 	{
 		float offset = GetBeltOffset( belt );
+		// This is all bad please fix this rework it
+		// make it work please
+		// Needs something to do with offsets, and try not to actually make entities for each item on the belt
+		// render separately
+		// maybe do it a less efficient way but update once every 3 frames? like minecraft with 20t/s
 
-		var newItem = TypeLibrary.Create<ModelEntity>( ClassName );
-		newItem.Position = Position + Rotation.Left * 0.5f;
+		//var newItem = TypeLibrary.Create<ModelEntity>( ClassName );
+		//newItem.Position = Position + Rotation.Left * 0.5f;
 	}
 
 
